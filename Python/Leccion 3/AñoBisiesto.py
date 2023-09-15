a31f23a21fffe1f56d5107e7f36222ce335b0816
uriel <urielpardod712@gmail.com>
2023-09-15 18:53:22 -0300

    Modificaciones en carpetas

--- Python/Leccion 3/AñoBisiesto.py
+++
@@ -1,8 +0,0 @@
-opcion = 0
-while (opcion != 1):
-    anio = int(input("Digite un año\n"))
-    if ((anio % 4 == 0) and (anio % 100 != 0) or (anio % 400 == 0)):
-        print("El año es bisiesto")
-    else:
-        print("El año no es bisiesto")
-    opcion = int(input("Para salir digite 1\n"))
